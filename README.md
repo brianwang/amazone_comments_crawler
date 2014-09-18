3f434ce601d015f2d69b647a421887b6bc27197a
brianwang <wangxydlut@gmail.com>
2014-09-19 01:02:22 +0800

    files upload
    commit files

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 amazone_comments_crawler
 ========================
 
-an amazone crawler by using scrapy
+An amazone crawler by using scrapy
+
+1. You should install Scrapy. pip is a better choice
+Ubuntu:
+sudo pip install Scrapy
+
+2. Create a project
+Scrapy startproject XXXX
+
+3. copy Commentspider.py to XXXX/spiders/
+
+4. You can list spider by using command
+scrapy list
+
+5. scrapy crawler comment -a itemid=XXXXXX (XXXX- is the amazone item id)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+import feedparser
+from twisted.internet import reactor
+from scrapy.crawler import Crawler
+from scrapy.settings import Settings
+from scrapy import log
+from cw_amazone.spiders.book_spider import BookSpider
+from cw_amazone.spiders.comment_spider import CommentSpider
+
+def setup_crawler(domain, spidername):
+    spider_class = globals()[spidername]
+    spider = spider_class(domain=domain)
+    crawler = Crawler(Settings())
+    crawler.configure()
+    crawler.crawl(spider)
+    crawler.start()
+#setup_crawler("amazone.com","BookSpider");
+
+python_wiki_rss_url = "http://www.amazon.cn/gp/rss/bestsellers/books/658409051/ref=zg_bs_658409051_rsslink"
+#"http://www.amazon.cn/%E5%B0%91%E5%84%BF%E5%9B%BE%E4%B9%A6/b/ref=sa_menu_books_l2_b658409051?ie=UTF8&node=658409051",
+
+feed = feedparser.parse( python_wiki_rss_url)
+
+surl = [];
+for item in feed['items']:
+    #print item['title']+'\n';
+        print item['link']+'\n';
+        ids =item['guid'].split('_');
+        print ids;
+        #大属性节点的标识（比如top3儿童）
+#	print ids[2];
+        #亚马逊商品的唯一ID
+	
+        surl.append("http://www.amazon.cn/product-reviews/"+ids[3]+"/ref=cm_cr_pr_top_link_"++"?ie=UTF8&pageNumber="++"&showViewpoints=0&sortBy=byRankDescending");
+
+print surl;
+spider = CommentSpider(domain="amazone.com")
+spider.start_urls = surl;
+crawler = Crawler(Settings())
+crawler.configure()
+crawler.crawl(spider)
+crawler.start()
+log.start()
+reactor.run() # the script will block here
