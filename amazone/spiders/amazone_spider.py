b59fb9709ddc3a0894ea35df23ce64366193677a
brianwang <wangxydlut@gmail.com>
2014-10-17 00:45:11 +0800

    亚马逊改版
    亚马逊相关内容插入数据库

--- amazone/spiders/amazone_spider.py
+++ amazone/spiders/amazone_spider.py
@@ -1,7 +1,9 @@
 # -*- coding=UTF-8 -*-
 import scrapy
+from scrapy.xlib.pydispatch import dispatcher
 from scrapy.contrib.loader import ItemLoader
 from scrapy.http import Request
+from scrapy.signalmanager  import SignalManager
 import json
 import codecs,sys
 import MySQLdb as mdb
@@ -10,40 +12,78 @@ import uuid
 #reload(sys)
 #sys.setdefaultencoding('utf-8')
 class AmazoneSpider(scrapy.Spider):
-        name = "amazone"
-        allowed_domains = ["amazone.cn"]
-        start_urls = []
-        savefile=''	
-        con = ''
-        def __init__(self,domain=None):
-                #self.con = mdb.connect('localhost', 'root', 'admin', 'huiben');
-                self.start_urls = ["http://www.amazon.cn/s?ie=UTF8&page=1&rh=n%3A658409051%2Cp_n_fulfilled_by_amazon%3A326314071"]
-                filename = str(uuid.uuid1());
-                print filename;
+	name = "amazone"
+	allowed_domains = ["amazone.cn"]
+	start_urls = []
+	savefile=''	
+	con = ''
+	cur = ''
+	def __init__(self,domain=None):
+		self.con = mdb.connect('localhost', 'root', 'admin', 'huiben');
+		self.cur=self.con.cursor()
+		self.start_urls = ["http://www.amazon.cn/s?ie=UTF8&page=1&rh=n%3A658409051%2Cp_n_fulfilled_by_amazon%3A326314071"]
+		filename = str(uuid.uuid1());
+		print filename;
 		self.savefile = codecs.open(filename,'w','utf-8')
-
+		SignalManager(dispatcher.Any).connect(self.closed_handler, signal=scrapy.signals.spider_closed);
+                
+	def closed_handler(self, spider):
+		print "close spider";
+		self.savefile .close();
+		self.cur.close();
+		self.con.close();
+	
+				
 	def parse(self, response):
-		 #title=response.css('div[id*=result_]').extract()
-		 #print title;
-		 #title=response.css('.newaps').extract()
-		 #title=response.xpath('//div[contains(@id,"result_")]').extract()
-		 title=response.xpath('//div[contains(@class,"productTitle")]/a/@href').extract()
-		 for t in title:
-			ridx = t.rfind('/');
+		title=response.xpath('//div[contains(@class,"productTitle")]/a/@href').extract()
+		for t in title:
+			ridx = t.rfind('/')+1;
 			aid = t[ridx:]
 			#print aid
 			#print ridx;
-			url= t[:ridx]+"/product-description"+aid
-			#print url;
-			yield Request(url, callback=self.parseBook,dont_filter=True)
-#		page=response.xpath('//span[contains(@class,"paging")]/a[last()-1]/text()').extract();	
-#		page =1
-	#	for p in range(1,page):
-		#	url = "http://www.amazon.cn/s/ref=lp_658409051_pg_"+str(p)+"?rh=n%3A658390051%2Cn%3A%21658391051%2Cn%3A658409051&page="+str(p)+"&ie=UTF8&qid=1412177207"
-			#print url;
-			#yield Request(url, callback=self.parseBook,dont_filter=True)
+			#url = t[:ridx]+"/"+aid;
+			url = 'http://www.amazon.cn/%E5%9B%BE%E4%B9%A6/dp/'+aid
+			#print url;			
+			yield Request(url, callback=self.parseBook,dont_filter=True,meta={"aid":aid,'url': t[:ridx]})
+			
+	def parseBook(self,response):
+		name = ''.join(response.css('span[id=btAsinTitle] span::text').extract()).strip(' \t\n\r')
+		print name
+		listprice= response.css('span[id=listPriceValue]::text').extract()[0][2:]
+		print listprice
+		price= response.css('span[id=actualPriceValue] b::text').extract()[0][2:]
+		print price
+		detail = response.css('td.bucket div.content ul li::text').extract()
+		print detail
+		#self.savefile.write(''.join(detail));
+		pub = detail[0]
+		print pub
+		pages = detail[1]
+		print pages
+		readerage =explode(detail[2][-1],'-')
+		reader_age_from = readerage[0]
+		print reader_age_from
+		reader_age_to = readerage[1]
+		print reader_age_to
+		lang = detail[3]
+		print lang
+		pagesize = detail[4]
+		isbn = detail[5]
+		size = detail[6]
+		rank = detail[11]
+		aid = response.meta['aid']
+		url= response.meta['url']+"product-description/"+aid
+		print url
+		sql = 'insert into amazone_book(id,name,realprice,price,\
+			reader_age_from,reader_age_to,pub,\
+			page,lang,isbn,rank,size,pagesize,\
+			content,) \
+			values(\''+aid+'\',\''+name+'\','+listprice+','+price+','+reader_age_from+','\
+				+reader_age_to+',\''+pub+'\','+page+',\''+lang+'\',\''+isbn+'\','+rank
+		yield Request(url, callback=self.parseBookDescription,dont_filter=True,meta={"aid":aid})
 
-	def parseBook(self, response):
+	def parseBookDescription(self, response):
+		aid = response.meta['aid']
 		name = ''.join(response.css('span[id=btAsinTitle] span::text').extract()).strip(' \t\n\r')
 		print name
 		listprice= response.css('span[id=listPriceValue]::text').extract()[0][2:]
@@ -52,8 +92,8 @@ class AmazoneSpider(scrapy.Spider):
 		print price
 		content =response.css('div[class=content]').extract()[0].replace('\'','"').strip(' \t\n\r')
 		#print content;
-		sql = 'insert into aaa values(\''+name+'\','+listprice+','+price+',\''+content+'\')'
+		sql = 'insert into amazone_book values(\''+aid+'\',\''+name+'\','+listprice+','+price+',\''+content+'\')'
 		self.savefile.write(sql);
-		
+		#self.cur.execute(sql)
 
 
